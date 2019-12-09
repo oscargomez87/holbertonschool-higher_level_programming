0710e7c3d553d3e4687cad33ed615ed892dc9167
oscargomez87 <oscarstiven87@gmail.com>
2019-12-09 17:43:13 +0000

    Task 10 - C program that checks if a linked list has a cycle And the files neccesary to test it

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a linked list has a cycle
+ *
+ * @list: linked list to check
+ * Return: 0 if there is no cycle, 1 otherwise
+ */
+int check_cycle(listint_t *list)
+{
+	const listint_t *node;
+
+	node = list;
+	while (node->next != NULL)
+	{
+		if (node->next == list)
+			return (1);
+		node = node->next;
+	}
+	return (0);
+}
