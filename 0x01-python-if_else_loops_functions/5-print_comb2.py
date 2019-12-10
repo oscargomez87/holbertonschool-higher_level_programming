41934eb11ed7ddbbfd1b964a5af012beb46b7b7a
oscargomez87 <oscarstiven87@gmail.com>
2019-12-10 17:23:50 +0000

    Formated string to print as a double digit using one loop

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(10):
-        if i == 9 and j == 9:
-            print("{:d}{:d}".format(i, j))
+for i in range(00, 100):
+        if i == 99:
+            print("{:02d}".format(i))
         else:
-            print("{:d}{:d}".format(i, j), end=', ')
+            print("{:02d}".format(i), end=', ')
