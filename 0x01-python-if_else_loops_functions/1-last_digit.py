26251f0f3def4cc0bdf45b9d2f083d14ef73ef6a
oscargomez87 <oscarstiven87@gmail.com>
2019-12-10 18:15:33 +0000

    Store last number in variable and made it negative if number is less than 0

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if abs(number) % 10 > 5:
+last_number = abs(number) % 10
+if number < 0:
+    last_number = last_number * -1
+if last_number > 5:
     print("Last digit of {:d} is {:d} and is greater than 5"
-          .format(number, abs(number) % 10))
-elif abs(number) % 10 == 0:
+          .format(number, last_number))
+elif last_number == 0:
     print("Last digit of {:d} is {:d} and is 0"
-          .format(number, abs(number) % 10))
-elif (abs(number) % 10 < 6) and (abs(number) % 10 != 0):
+          .format(number, last_number))
+elif last_number < 6 and last_number != 0:
     print("Last digit of {:d} is {:d} and is less than 6 and not 0"
-          .format(number, abs(number) % 10))
+          .format(number, last_number))
