71e8aab98cba6e41b350a1913b9079052fbfdaf5
oscargomez87 <oscarstiven87@gmail.com>
2019-12-10 17:15:23 +0000

    Changed print format from one digit to two digit

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i != 99:
-        print("{:d}".format(i), end=', ')
-    else:
-        print("{:d}".format(i))
+for i in range(10):
+    for j in range(10):
+        if i == 9 and j == 9:
+            print("{:d}{:d}".format(i, j))
+        else:
+            print("{:d}{:d}".format(i, j), end=', ')
