5959f44c47e24be67c0b3035be3b5401b99dbb2b
oscargomez87 <oscarstiven87@gmail.com>
2019-12-10 19:30:33 +0000

    Change check order

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -23,15 +23,15 @@ listint_t *insert_node(listint_t **head, int number)
 	{
 		if (temp->next != NULL)
 		{
-			if (temp->n < number && temp->next->n > number)
-			{
-				newnode->next = temp->next;
-				temp->next = newnode;
-				break;
-			}else if (temp->n > number)
+			if (temp->n > number)
 			{
 				newnode->next = temp;
 				*head = newnode;
+				break;
+			}else if (temp->next->n > number)
+			{
+				newnode->next = temp->next;
+				temp->next = newnode;
 				break;
 			}
 		}else
