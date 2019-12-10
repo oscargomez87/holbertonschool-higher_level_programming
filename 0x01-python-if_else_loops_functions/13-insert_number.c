671e54f98f78b0576fd186486b98f47605c86afb
oscargomez87 <oscarstiven87@gmail.com>
2019-12-10 19:36:59 +0000

    Fixed check for NULL argument

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -11,7 +11,7 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *newnode, *temp;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return (NULL);
 	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
