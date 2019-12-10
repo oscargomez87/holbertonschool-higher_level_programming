3bc55cc219abf66f48297785c5e7724a1b40f6ad
oscargomez87 <oscarstiven87@gmail.com>
2019-12-10 17:38:09 +0000

    Restored random number generation

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import random
-number = -9200
+number = random.randint(-10000, 10000)
 if abs(number) % 10 > 5:
     print("Last digit of {:d} is {:d} and is greater than 5"
           .format(number, abs(number) % 10))
