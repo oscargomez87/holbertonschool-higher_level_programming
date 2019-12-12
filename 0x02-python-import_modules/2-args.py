d56ef6413c3e000a684249d6cd373d7a355ebcbc
oscargomez87 <oscarstiven87@gmail.com>
2019-12-12 17:22:35 +0000

    Fixed wrong output separating if and adding an elif statement

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    print("{:d} arguments.".format(len(argv[1:])) if len(argv[1:]) != 1
-          else "{:d} argument:".format(len(argv[1:])))
+    if len(argv[1:]) == 0:
+        print("{:d} arguments.".format(len(argv[1:])))
+    elif len(argv[1:]) == 1:
+        print("{:d} argument:".format(len(argv[1:])))
+    else:
+        print("{:d} arguments:".format(len(argv[1:])))
     for index, element in enumerate(argv[1:], start=1):
         print("{:d}: {}".format(index, element))
