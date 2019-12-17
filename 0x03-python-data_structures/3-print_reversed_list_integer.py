4b3e56c6ba32daa999dc85cd4c8ceb766316ef8d
oscargomez87 <oscarstiven87@gmail.com>
2019-12-17 17:19:26 +0000

    Handles empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for n in reversed(my_list):
-        print("{:d}".format(n))
+    if my_list:
+        for n in reversed(my_list):
+            print("{:d}".format(n))
+    else:
+        print()
