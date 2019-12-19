e5e8f1456fb5218c8d6be5e433582a58ae2b1fb8
oscargomez87 <oscarstiven87@gmail.com>
2019-12-19 16:32:58 +0000

    Added check for empty dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,5 @@
 def print_sorted_dictionary(a_dictionary):
-    list_keys = sorted(a_dictionary.keys())
-    for e in list_keys:
-        print("{}: {}".format(e, a_dictionary[e]))
+    if a_dictionary:
+        list_keys = sorted(a_dictionary.keys())
+        for e in list_keys:
+            print("{}: {}".format(e, a_dictionary[e]))
