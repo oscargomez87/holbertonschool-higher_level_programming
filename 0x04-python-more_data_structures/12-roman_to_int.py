ebabcb8a284dfe4eaeab37131ab32bae1e231744
oscargomez87 <oscarstiven87@gmail.com>
2019-12-19 19:28:52 +0000

    Check if is roman number

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,8 @@
 def roman_to_int(roman_string):
     if not roman_string or roman_string == "None":
         return 0
+    if not hasattr(roman_string, "__iter__"):
+        return 0
     romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = []
     resum = 0
