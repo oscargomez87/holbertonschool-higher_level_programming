fd398361517eab8ae35699cf65f89288ae107611
oscargomez87 <oscarstiven87@gmail.com>
2019-12-19 19:04:47 +0000

    Removed unnecesary prints

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -9,13 +9,9 @@ def roman_to_int(roman_string):
         for c in list(romans.keys()):
             if e == c:
                 result.append(romans[e])
-    print(result)
     for idx, e in enumerate(result):
-        print(e)
         if e <= result[idx - 1]:
             resum = resum + e
-            print(resum)
         else:
             resum = e - resum
-            print(resum)
     return resum
