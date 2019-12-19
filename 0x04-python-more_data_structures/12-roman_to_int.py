bdb7a5caa9d49e865ec441e4ab07380f1c1f0c81
oscargomez87 <oscarstiven87@gmail.com>
2019-12-19 20:07:59 +0000

    Reworked long code in a shorter and more accurete one checks if position is bigger than 0 and if value of actual roman number is less than the value in the position before, if true substracts twice the last position from the actual position and adds the value to resum otherwise adds the value in the actual position to resum

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,23 +5,10 @@ def roman_to_int(roman_string):
     if not hasattr(roman_string, "__iter__"):
         return 0
     romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    result = []
     resum = 0
-    for e in roman_string:
-        for c in list(romans.keys()):
-            if e == c:
-                result.append(romans[e])
-    if len(result) < 2:
-        return result[0]
-    for idx, e in enumerate(result):
-        if idx < len(result) - 1:
-            if e >= result[idx + 1]:
-                resum = resum + e
-            else:
-                if e > resum:
-                    resum = e - resum
-                else:
-                    resum = resum - e
+    for st in range(len(roman_string)):
+        if st > 0 and romans[roman_string[st]] > romans[roman_string[st - 1]]:
+            resum += romans[roman_string[st]] - 2 * romans[roman_string[st - 1]]
         else:
-            resum = resum + e
+            resum += romans[roman_string[st]]
     return resum
