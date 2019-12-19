f577ecff45a7de7770c1d3fc2c8db9d3155b7dbf
oscargomez87 <oscarstiven87@gmail.com>
2019-12-19 19:03:34 +0000

    Fixed pep8 compliance

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if not roman_string or roman_string == "None":
         return 0
-    romans = {'I': 1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = []
     resum = 0
     for e in roman_string:
