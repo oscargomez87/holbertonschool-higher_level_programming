a34b9b9e861e6a00f168bc58e042986d3b22ff94
oscargomez87 <oscarstiven87@gmail.com>
2020-01-08 20:00:48 +0000

    Empty prototypes for task 9 of project 0x05. Python - Exceptions

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,32 @@
+#include <Python.h>
+#include <stdio.h>
+
+/**
+ * print_python_list - Prints python lists objects information
+ *
+ * @p: python object to print information from
+ */
+void print_python_list(PyObject *p)
+{
+	(void) *p;
+}
+
+/**
+ * print_python_bytes - Prints python bytes objects information
+ *
+ * @p: python object to print information from
+ */
+void print_python_bytes(PyObject *p)
+{
+	(void) *p;
+}
+
+/**
+ * print_python_float - Prints python float objects information
+ *
+ * @p: python object to print information from
+ */
+void print_python_float(PyObject *p)
+{
+	(void) *p;
+}
