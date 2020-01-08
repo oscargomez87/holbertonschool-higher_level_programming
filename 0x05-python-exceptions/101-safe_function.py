8bf471b4cecf4b633038597f49b4cd44966389a8
oscargomez87 <oscarstiven87@gmail.com>
2020-01-08 18:09:57 +0000

    Added Task 8 for project 0x05. Python - Exceptions

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/pyton3
+def safe_function(fct, *args):
+    from sys import stderr
+    try:
+        a = fct(*args)
+        return a
+    except Exception as e:
+        print("Exception: {}".format(e.args[0]), file=stderr)
