d4f62f3fd47612d081da4a3a62b6ad870eb5908c
oscargomez87 <oscarstiven87@gmail.com>
2020-01-08 17:46:51 +0000

    Added Task 7 for project 0x05. Python - Exceptions

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from sys import stderr
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as e:
+        print("Exception: {}".format(e.args[0]), file=stderr)
