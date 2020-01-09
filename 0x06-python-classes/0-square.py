ce86b4b79e0aca6e6f49edad06d994b3913f9d3a
oscargomez87 <oscarstiven87@gmail.com>
2020-01-09 15:35:36 +0000

    Task 2 for project 0x06. Python - Classes and Objects

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module defines an empty square.
+
+This module demonstrates the creation of classes.
+"""
+class Square():
+    """Empty class.
+
+    Defines a square class.
+    """
+    pass
