5450175d3c72bc4d327d2d7acc815cfb62a52b3e
oscargomez87 <oscarstiven87@gmail.com>
2020-01-09 18:41:29 +0000

    Task 4 for project 0x06. Python - Classes and Objects

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""This module defines a square class."""
+
+
+class Square:
+    """Defines a square of __size as size.
+    Attributes:
+    size (int): size of the square.
+    """
+
+    def __init__(self, size=0):
+        """__init__ method for the Square class.
+        Args:
+        size (int): Size of the square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieves the size of the square.
+
+        The setter method checks the value and if is not an integer raises
+        a TyperError, if size is less than 0 raises a ValueError.
+
+        Returns:
+        int: size of square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        try:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
+        except TypeError:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """Defines the area of the square
+
+        Returns:
+        int: Area of the square
+        """
+        a = self.__size ** 2
+        return a
