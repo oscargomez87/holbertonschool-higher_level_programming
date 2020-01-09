907b0fde69f08b6ea7c5320647bfe62e2a41d7e2
Oscar Gomez Toro <oscarstiven87@gmail.com>
2020-01-09 22:50:02 +0000

    Changed checks order

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -29,10 +29,10 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if value < 0:
-            raise ValueError("size must be >= 0")
         if type(value) != int:
             raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
