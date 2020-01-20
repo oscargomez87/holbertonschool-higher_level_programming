6ddb3f29322ae44125d2d36d3d182631cd1637b7
oscargomez87 <oscarstiven87@gmail.com>
2020-01-20 15:44:49 +0000

    Function prints attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Method for task 0"""
+
+
+def lookup(obj):
+    """Returns a list of the attributes and methods of an object
+
+    Atr.
+    obj: object to print information from.
+    """
+    return dir(obj)
