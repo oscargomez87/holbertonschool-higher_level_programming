c9b2456a32e8b46ea90a306d6bb580f0f130f4e7
Oscar Gomez Toro <oscarstiven87@gmail.com>
2020-01-28 01:36:16 +0000

    Added new tests

--- 0x0C-python-almost_a_circle/tests/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -9,3 +9,12 @@ class TestBase(unittest.TestCase):
     def test_doc(self):
         """Testing if doc exist """
         self.assertTrue(len(Square.__doc__) > 0)
+
+    def test_id(self):
+        """testing id counter and assignment"""
+        s1 = Square(10, 2)
+        s2 = Square(2, 10)
+        s3 = Square(10, 0, 0, 12)
+        self.assertAlmostEqual(s1.id, 8)
+        self.assertAlmostEqual(s2.id, 9)
+        self.assertAlmostEqual(s3.id, 12)
