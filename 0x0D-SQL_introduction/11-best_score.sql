74af40e8c9dcf6922a7ed840cf8809207597a4d5
oscargomez87 <oscarstiven87@gmail.com>
2020-03-02 17:37:10 -0500

    Lists records with a score >= 10 of the table second_table of the database hbtn_0c_0 Only score and name with descending order by score

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- Lists records with a score >= 10 of the table second_table of the database hbtn_0c_0
+-- Only score and name with descending order by score
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC
