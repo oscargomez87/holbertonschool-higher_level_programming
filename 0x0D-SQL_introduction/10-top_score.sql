d5c173ecde4c040243a9d418d77d440fb9994e28
oscargomez87 <oscarstiven87@gmail.com>
2020-03-02 17:34:53 -0500

    Lists all records of the table second_table of the database hbtn_0c_0 Only score and name with descending order by score

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- Lists all records of the table second_table of the database hbtn_0c_0
+-- Only score and name with descending order by score
+SELECT score, name FROM second_table ORDER BY score DESC
