e517e7f531dd44f4df748dc6e693ace25384e79a
oscargomez87 <oscarstiven87@gmail.com>
2020-03-03 11:22:29 -0500

    Creates the table unique_id on your MySQL server with the fields id of type int default value 1 and name of type varchar max 256 chars

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,4 @@
+-- Creates the table unique_id on your MySQL server
+-- with the fields id of type int default value 1
+-- and name of type varchar max 256 chars
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256))
