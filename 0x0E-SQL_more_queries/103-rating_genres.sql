0164f38fbcf8e5a16221358fb396a7d62941d806
oscargomez87 <oscarstiven87@gmail.com>
2020-03-03 17:36:09 -0500

    Changed keyword from lowercase to uppercase

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,5 +1,5 @@
--- lists all genres in the database hbtn_0d_tvshows_rate by their rating
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) as rating
+-- Lists all genres in the database hbtn_0d_tvshows_rate by their rating
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
   FROM tv_shows
        INNER JOIN tv_show_genres
        ON tv_shows.id = tv_show_genres.show_id
