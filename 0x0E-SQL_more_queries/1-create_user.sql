0790ab1d05b1b7189906bfaf7895a131c6bccd5c
oscargomez87 <oscarstiven87@gmail.com>
2020-03-03 12:12:51 -0500

    Added passwrod for user created

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Creates the MySQL server user user_0d_1 with all privileges
--- password is not set and if the user already exists it's skipped
+-- password is set to user_0d_1_pwd and if the user already exists it's skipped
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
