340da6f1877517f508f36c134da97caf1c42d2a9
oscargomez87 <oscarstiven87@gmail.com>
2020-03-28 10:11:24 -0500

    Lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa"""
+
+from sys import argv
+import MySQLdb
+
+
+def main():
+    """maint method of module"""
+
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3],
+                         port=3306, host="localhost")
+    qry = db.cursor()
+    qry.execute("""SELECT * FROM states ORDER BY states.id ASC""")
+    rows = qry.fetchall()
+    for r in rows:
+        print(r)
+
+if __name__ == "__main__":
+    main()
