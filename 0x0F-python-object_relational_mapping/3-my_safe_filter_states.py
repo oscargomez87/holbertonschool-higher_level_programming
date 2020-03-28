e38850f18f9855d0839ec6d625ab48dd732e723a
oscargomez87 <oscarstiven87@gmail.com>
2020-03-28 10:59:17 -0500

    Copy of 2-my_filter_states.py but this time safe of SQL injections

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument."""
+
+import MySQLdb
+from sys import argv
+
+
+def main():
+    """main method of module"""
+
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cqry = db.cursor()
+    name = argv[4]
+    cqry.execute("""SELECT * FROM states WHERE name = %s""", (name,))
+    rows = cqry.fetchall()
+    for r in rows:
+        print(r)
+    cqry.close()
+    db.close()
+
+if __name__ == "__main__":
+    main()
