b21d19c686ec52832263af21830975aa498a0f50
oscargomez87 <oscarstiven87@gmail.com>
2020-03-28 12:07:45 -0500

    Receives a state as argument then lists all cities of that state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""script that takes in the name of a state as an argument and lists
+all cities of that state, using the database hbtn_0e_4_usa"""
+
+import MySQLdb
+from sys import argv
+
+
+def main():
+    """main method"""
+
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    s_name = argv[4]
+    cqry = db.cursor()
+    cqry.execute("""SELECT c.name
+    FROM states AS s
+    INNER JOIN cities AS c
+    WHERE s.name = %s AND s.id = c.state_id""", (s_name,))
+    res = ''
+    rows = cqry.fetchall()
+    r_len = len(rows) - 1
+    for idx, row in enumerate(rows):
+        if idx < r_len:
+            res = res + ''.join(row) + ', '
+        else:
+            res = res + ''.join(row)
+    print(res)
+    cqry.close()
+    db.close()
+
+if __name__ == "__main__":
+    main()
