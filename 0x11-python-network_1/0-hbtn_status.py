dd7abe5d958c3ad42bd77f337d805c7ef1ff8e2b
oscargomez87 <oscarstiven87@gmail.com>
2020-04-14 16:13:15 -0500

    Fetchs web page

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Fetchs web page status"""
+import urllib.request
+
+req = urllib.request.Request('https://intranet.hbtn.io/status')
+with urllib.request.urlopen(req) as response:
+    html = response.read()
+print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+      .format(type(html), html, html.decode("utf-8")))
