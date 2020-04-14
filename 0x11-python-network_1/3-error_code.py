069ad3baf7d3e170c931f20c930d5f3988534ae0
oscargomez87 <oscarstiven87@gmail.com>
2020-04-14 16:14:07 -0500

    Fethcs url handling http error codes

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""fethcs url handling http error codes"""
+import urllib.request
+import urllib.parse
+from sys import argv
+
+
+url = argv[1]
+try:
+    with urllib.request.urlopen(url) as response:
+        print(response.read().decode('utf-8'))
+except urllib.error.HTTPError as e:
+    print("Error code: {}".format(e.code))
