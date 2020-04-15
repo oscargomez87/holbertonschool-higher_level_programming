a40acb282ba5419b41cba7c29152f41a9af030d7
oscargomez87 <oscarstiven87@gmail.com>
2020-04-15 14:07:21 -0500

    MAde not executable when imported

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,7 +3,6 @@
 import urllib.request
 import sys
 
-
-with urllib.request.urlopen(sys.argv[1]) as response:
-    html = response.info()
-print(html["X-Request-Id"])
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.info()["X-Request-Id"])
