a896c029fc3954952a3bb95ba61197c2ddbf8f1e
oscargomez87 <oscarstiven87@gmail.com>
2020-05-05 14:35:15 -0500

    Syntax fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const array = process.argv.slice(2).sort(function (a, b) { return a - b });
+const array = process.argv.slice(2).sort(function (a, b) { return a - b; });
 const aLength = array.length;
 if (aLength <= 1) {
   console.log(0);
