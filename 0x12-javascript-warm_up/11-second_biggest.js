6c56d46659bc04111f5cbae1691ec5fc094c4100
oscargomez87 <oscarstiven87@gmail.com>
2020-05-05 14:33:38 -0500

    Changed lexicographical sort for a numerical sort

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const array = process.argv.slice(2);
+const array = process.argv.slice(2).sort(function (a, b) { return a - b });
 const aLength = array.length;
 if (aLength <= 1) {
   console.log(0);
 } else {
-  console.log(array.sort()[aLength - 2]);
+  console.log(array[aLength - 2]);
 }
