9bf1a36daa29f4915bbdb70d470159bcd58d96a1
oscargomez87 <oscarstiven87@gmail.com>
2020-05-05 13:21:18 -0500

    Removed duplicates from array

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-const array = process.argv.slice(2);
+const args = process.argv.slice(2);
+const array = [...new Set(args)];
 const aLength = array.length;
 if (aLength <= 1) {
   console.log(0);
 } else {
-  console.log(array.sort()[aLength - 2]);
+  console.log(array[aLength - 2]);
 }
