15abe2c43715979ac4c911afaf605f54b4496fd2
oscargomez87 <oscarstiven87@gmail.com>
2020-05-05 14:17:16 -0500

    roll changes back to first commit

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const array = [...new Set(process.argv.slice(2))];
+const array = process.argv.slice(2);
 const aLength = array.length;
 if (aLength <= 1) {
-  console.log('0');
+  console.log(0);
 } else {
-  console.log(array.sort()[array.length - 2]);
+  console.log(array.sort()[aLength - 2]);
 }
