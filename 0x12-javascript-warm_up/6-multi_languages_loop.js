6dcbaf43741bc76d19496dc945c894b987a202a4
oscargomez87 <oscarstiven87@gmail.com>
2020-05-05 11:26:13 -0500

    JS script that prints three strings

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let step = 0;
+while (step < strings.length) {
+  console.log(strings[step]);
+  step++;
+}
