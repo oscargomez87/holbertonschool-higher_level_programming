8b870f5d3eb51a490353d666360d9ed5fed5481e
oscargomez87 <oscarstiven87@gmail.com>
2020-05-11 15:51:15 -0500

    Added methods double and rotate

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let step = 0; step < this.height; step++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+};
