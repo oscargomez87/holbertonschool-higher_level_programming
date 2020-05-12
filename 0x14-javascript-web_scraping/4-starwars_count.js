2edb2de579ed49e491fe2716495aefa024d2d1f0
oscargomez87 <oscarstiven87@gmail.com>
2020-05-12 17:49:19 -0500

    Using regex for character search

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-const charUrl = 'https://swapi-api.hbtn.io/api/people/' + '18/';
+const reqChar = /18\/$/;
 
 request.get(url, (err, response, body) => {
   if (err) {
@@ -11,9 +11,11 @@ request.get(url, (err, response, body) => {
     const jsonObject = JSON.parse(body).results;
     if (jsonObject !== undefined) {
       for (let step = 0; step < jsonObject.length; step++) {
-        if (jsonObject[step].characters.includes(charUrl)) {
-          numMovies++;
-        }
+        jsonObject[step].characters.forEach(character => {
+          if (character.search(reqChar) !== -1) {
+            numMovies++;
+          }
+        });
       }
     }
     console.log(numMovies);
