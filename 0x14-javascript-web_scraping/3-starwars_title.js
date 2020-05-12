d334699f88fe768948f8fcb439d32650dc4cde8f
oscargomez87 <oscarstiven87@gmail.com>
2020-05-12 14:50:07 -0500

    Removed verb

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request.get(url, (err, response, body) => {
-  console.log(JSON.parse(body).title);
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
 });
