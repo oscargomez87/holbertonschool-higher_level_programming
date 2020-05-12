ea672bbcdcf5def9f036a68484a6407fc8e9065a
oscargomez87 <oscarstiven87@gmail.com>
2020-05-12 14:41:57 -0500

    Removed error handling

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
 request.get(url, (err, response, body) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(JSON.parse(body).title);
-  }
+  console.log(JSON.parse(body).title);
 });
