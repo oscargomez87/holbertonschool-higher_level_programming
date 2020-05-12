bc975ab26a690db6ed92af8cffae55854bd467b4
oscargomez87 <oscarstiven87@gmail.com>
2020-05-12 15:05:55 -0500

    print error in stderr

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,7 +4,7 @@ const url = 'https://swapi-api.hbtn.io/api/films/' + parseInt(process.argv[2]);
 
 request(url, (err, response, body) => {
   if (err) {
-    console.log(err);
+    console.error(err);
   }
   console.log(JSON.parse(body).title);
 });
