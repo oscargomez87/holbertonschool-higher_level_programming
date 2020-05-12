15c9d37b1376058c7062a5b6670ab34658bafcee
oscargomez87 <oscarstiven87@gmail.com>
2020-05-12 14:56:38 -0500

    Parse argument as int

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + parseInt(process.argv[2]);
 
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, (err, response, body) => {
+request.get(url, (err, response, body) => {
   if (err) {
     console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
   }
-  console.log(JSON.parse(body).title);
 });
