26e50e1e5e5e3e7ddeed5921c1dc23baae7fc7b9
oscargomez87 <oscarstiven87@gmail.com>
2020-05-14 15:26:26 -0500

    Script that fetches a character from the starwars API and displays it

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,9 @@
+$(function () {
+  $.ajax({
+    type: 'GET',
+    url: 'https://swapi-api.hbtn.io/api/people/5/?format=json',
+    success: function (character) {
+      $('div#character').text(character.name);
+    }
+  });
+});
